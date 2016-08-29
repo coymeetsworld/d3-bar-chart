b2d00eff1c4ff72ca278325d9505c174a9d098c5
Coy Sanders <coymeetsworld@gmail.com>
2016-08-28 18:34:32 -0700

    DataVis: BarChart: Turned bar chart to column

--- js/barchart.js
+++ js/barchart.js
@@ -1,34 +1,33 @@
 $(document).ready(function() {
 
   var chartWidth = 1200;
-  var barHeight = 20;
-  var scaleDisplay = d3.scaleLinear().range([0, chartWidth]);
-  var chart = d3.select('.chart').attr("width", chartWidth);
+  var chartHeight = 1200;
 
-
+  var scaleDisplay = d3.scaleLinear().range([chartHeight, 0]);
+  var chart = d3.select('.chart').attr("width", chartWidth).attr("height", chartHeight);
 
   $.getJSON('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', function(GDPdata) {
-
 	var max = d3.max(d3.values(GDPdata.data));
-
 	console.log("MAX GDP2: " + max[1]);
-	scaleDisplay.domain([0, d3.max(d3.values(GDPdata.data))[1] ]);
-	chart.attr("height", barHeight * GDPdata.data.length);
 
+	scaleDisplay.domain([0, d3.max(d3.values(GDPdata.data))[1] ]);
+
+	var barWidth = chartWidth / GDPdata.data.length;
 
   	var bar = chart.selectAll("g")
 	               .data(GDPdata.data)
 				   .enter().append("g")
-      		       .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+      		       .attr("transform", function(d, i) { return "translate(" + i * barWidth + ", 0)"; });
 
     bar.append("rect")
-       .attr("width", function(d) { console.log(d); return scaleDisplay(d[1]); })
-       .attr("height", barHeight - 1);
+	   .attr("y", function(d) { return scaleDisplay(d[1]); })
+       .attr("height", function(d) { return chartHeight - scaleDisplay(d[1]); })
+       .attr("width", barWidth - 1);
 	
   	bar.append("text")
-       .attr("x", function(d) { return scaleDisplay(d[1]) - 3; })
-       .attr("y", barHeight / 2)
-       .attr("dy", ".35em")
+       .attr("x", barWidth / 2)
+	   .attr("y", function(d) { return scaleDisplay(d[1]) + 3; })
+       .attr("dy", ".75em")
        .text(function(d) { return d[1]; });
   });
 });
