1adcf562348e9bdf3975e27465a08e240bc37a6d
Coy Sanders <coymeetsworld@gmail.com>
2016-08-30 20:55:25 -0700

    DataVis: BarChart: Added y-axis, bars rendered but needs adjustments.

--- js/barchart.js
+++ js/barchart.js
@@ -1,22 +1,18 @@
 $(document).ready(function() {
 
   var margin = {top: 20, right: 20, bottom: 50, left: 70}
-  var chartWidth = 1000 - margin.left - margin.right;
-  var chartHeight = 1000 - margin.top - margin.bottom;
+  var chartWidth = 1400 - margin.left - margin.right;
+  var chartHeight = 600 - margin.top - margin.bottom;
 
   var parseTime = d3.timeParse("%Y-%m-%d");
 
   //var x = d3.scaleLinear().range([chartHeight, 0]);
   var x = d3.scaleTime().range([0, chartWidth]);
+  var y = d3.scaleLinear().range([chartHeight, 0]);
 
-
-  var valueLine = d3.line()
-  					.x(function(d) { console.log("d: " + d); return x(d[0]); });
 
   //var chart = d3.select('.chart').attr("width", chartWidth+margin.left+margin.right).attr("height", chartHeight+margin.top+margin.bottom);
   var chart = d3.select('.chart').attr("width", chartWidth+margin.left+margin.right).attr("height", chartHeight+margin.top+margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
 
   d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', function(error, gdpData) {
 	if (error) throw error;
@@ -28,11 +24,7 @@ $(document).ready(function() {
 	});
 
 	x.domain(d3.extent(gdpData.data, function(d) { return d[0]; }));
-	//Add valueline path
-	chart.append("path")
-       .data([gdpData])
-	   .attr("class", "line")
-	   .attr("d", valueLine);     
+	y.domain([0, d3.max(gdpData.data, function(d) { return d[1]; } ) ]);
 
 	//Add x-axis
 	chart.append("g")
@@ -45,8 +37,20 @@ $(document).ready(function() {
 		 .style("text-anchor", "middle")
 		 .text("Year");
 
+    //Add y-axis
+    chart.append("g")
+		 .call(d3.axisLeft(y));
 
-		 /*
+    // text label for the y axis
+    chart.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x", 0 - (chartHeight / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("GDP"); 
+
+		 
 	var max = d3.max(d3.values(gdpData.data));
 	var barWidth = chartWidth / gdpData.data.length;
   	var bar = chart.selectAll("g")
@@ -63,7 +67,7 @@ $(document).ready(function() {
        .attr("x", barWidth / 2)
 	   .attr("y", function(d) { return x(d[1]) + 3; })
        .attr("dy", ".75em")
-       .text(function(d) { return d[1]; });*/
+       .text(function(d) { return d[1]; });
   });
 
 });
