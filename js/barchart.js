5ef5171f321effa45dc59bbf817bd3d1b69e793d
Coy Sanders <coymeetsworld@gmail.com>
2016-09-04 00:28:24 -0700

    DataVis: BarChart: Added tooltip on hover of bar.

--- js/barchart.js
+++ js/barchart.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+  function renderTooltip(x, y,evt2)
+  {
+	console.log(x + " " + y);
+	console.log(evt2);
+    tooltip.setAttributeNS(null,"x",x+30);
+    tooltip.setAttributeNS(null,"y",y+5);
+    tooltip.setAttributeNS(null,"visibility","visible");
+  }
+
   var margin = {top: 20, right: 30, bottom: 30, left: 40}
   var chartWidth = 1400 - margin.left - margin.right;
   var chartHeight = 600 - margin.top - margin.bottom;
@@ -12,7 +21,9 @@ $(document).ready(function() {
   var chart = d3.select('.chart').attr("width", 1400).attr("height", 600).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', function(error, gdpData) {
+	
 	if (error) throw error;
+
 
 	gdpData.data.forEach(function(d) {
 		d[0] = parseTime(d[0]);
@@ -26,12 +37,14 @@ $(document).ready(function() {
   	var bar = chart.selectAll("g")
 	               .data(gdpData.data)
 				   .enter().append("rect")
+				   .attr("class", "tooltip")
+				   .attr("title", "GDP")
 				   .attr("x", function(d) { return x(d[0]); })
 				   .attr("y", function(d) { return y(d[1]); })
 				   .attr("height", function(d) { return chartHeight - y(d[1]); })
 				   .attr("width", barWidth - 2)
 				   .on("mouseover", function() {
-					 console.log(d3.select(this).datum());
+		             renderTooltip(this.x.baseVal.value, this.y.baseVal.value, d3.select(this).datum());
 				   });
 
 	//Add x-axis
